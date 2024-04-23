a9045fc541879a05084f711ef3ac079fb52453b2
Ivan <fedorchuk.kp93@gmail.com>
2024-04-23 16:11:23 +0300

    Refactored aes ecb deciphering function

--- cryptopals/Set1/aes-in-ecb-mode.js
+++ cryptopals/Set1/aes-in-ecb-mode.js
@@ -4,7 +4,13 @@ const CryptoJS = require('crypto-js');
 function AES_ECB_decipher(key, cipherText) {
     const local_key = CryptoJS.enc.Utf8.parse(key);
     const input = CryptoJS.enc.Hex.parse(cipherText);
-    const result = CryptoJS.AES.decrypt({ ciphertext: input }, local_key, { mode: CryptoJS.mode.ECB, padding: CryptoJS.pad.Pkcs7 }).toString();
+    
+    const result = CryptoJS.AES.decrypt(
+        { ciphertext: input },
+        local_key,
+        { mode: CryptoJS.mode.ECB, padding: CryptoJS.pad.Pkcs7 }
+    ).toString();
+
     return Buffer.from(result, 'hex').toString('utf-8');
 }
 
