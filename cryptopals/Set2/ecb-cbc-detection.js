fd8b5c64f5cd8476b9e29d67531fe7d2f6159887
Ivan <fedorchuk.kp93@gmail.com>
2024-04-24 14:39:54 +0300

    Implemented detection oracle ecb/cbc

--- cryptopals/Set2/ecb-cbc-detection.js
+++ cryptopals/Set2/ecb-cbc-detection.js
@@ -7,7 +7,7 @@ function randomBytesGeneration(bytes_n = 16) {
     return crypto.randomBytes(bytes_n);
 }
 
-function encryptionOracle(message, AES_mode) {
+function encryptionOracle(message, AES_mode = crypto.randomInt(2)) {
     const key = randomBytesGeneration();
     const keyWA = convert_to_word_array(key);
 
@@ -31,9 +31,33 @@ function encryptionOracle(message, AES_mode) {
         }
     ];
 
-    return ECB_CBC_result[AES_mode ?? crypto.randomInt(2)];
+    return ECB_CBC_result[AES_mode]();
+}
+
+function detectionOracle_ECB_CBC(cipherText, encoding = 'hex') {
+    const cipherBuffer = Buffer.from(cipherText, encoding);
+
+    let score = 0;
+
+    for(let offset = 5; offset <= 10; offset++) {
+        const statistic = {};
+        
+        for(let i = offset; i < cipherBuffer.length - 15; i += 16) {
+            const block = cipherBuffer.subarray(i, i+16).toString('base64');
+            statistic[block] = (
+                statistic[block] !== undefined ?
+                statistic[block] + 1 :
+                0
+            );
+        }
+
+        score = Math.min(score, Object.values(statistic).reduce((sum, val) => sum + val));
+    }
+
+    return score > 0 ? 'ECB' : 'CBC';
 }
 
 module.exports = {
-    encryptionOracle
+    encryptionOracle,
+    detectionOracle_ECB_CBC
 };
