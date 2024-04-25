57274be1ec690544e6883f819bdac18dd8f92ce7
Ivan <fedorchuk.kp93@gmail.com>
2024-04-25 14:31:15 +0300

    Fixed ECB/CBC detection function

--- cryptopals/Set2/ecb-cbc-detection.js
+++ cryptopals/Set2/ecb-cbc-detection.js
@@ -37,22 +37,18 @@ function encryptionOracle(message, AES_mode = crypto.randomInt(2)) {
 function detectionOracle_ECB_CBC(cipherText, encoding = 'hex') {
     const cipherBuffer = Buffer.from(cipherText, encoding);
 
-    let score = 0;
-
-    for(let offset = 5; offset <= 10; offset++) {
-        const statistic = {};
-        
-        for(let i = offset; i < cipherBuffer.length - 15; i += 16) {
-            const block = cipherBuffer.subarray(i, i+16).toString('base64');
-            statistic[block] = (
-                statistic[block] !== undefined ?
-                statistic[block] + 1 :
-                0
-            );
-        }
-
-        score = Math.max(score, Object.values(statistic).reduce((sum, val) => sum + val));
+    const statistic = {};
+    
+    for(let i = 0; i < cipherBuffer.length - 15; i += 16) {
+        const block = cipherBuffer.subarray(i, i+16).toString('base64');
+        statistic[block] = (
+            statistic[block] !== undefined ?
+            statistic[block] + 1 :
+            0
+        );
     }
+
+    const score = Object.values(statistic).reduce((sum, val) => sum + val);
 
     return score > 0 ? 'ECB' : 'CBC';
 }
