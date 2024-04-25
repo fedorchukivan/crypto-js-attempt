55173b5151911886303615fa0b97c71c5450c2bd
Ivan <fedorchuk.kp93@gmail.com>
2024-04-25 15:35:45 +0300

    Implemented function for finding block size of encryption oracles, based on block ciphers

--- cryptopals/Set2/simple-byte-at-a-time-ecb-decription.js
+++ cryptopals/Set2/simple-byte-at-a-time-ecb-decription.js
@@ -4,20 +4,42 @@ const { convert_to_word_array } = require('./helpers');
 const { AES_ECB_cipher } = require('./cbc-mode');
 
 function encryptionOracleFactoryECB(appendage, keySize = 16) {
-  const key = crypto.randomBytes(keySize);
-  const keyWA = convert_to_word_array(key);
+    const key = crypto.randomBytes(keySize);
+    const keyWA = convert_to_word_array(key);
 
-  return function (message) {
-    const appendageBytes = Buffer.from(appendage, 'base64');
-    const messageBytes = Buffer.from(message, 'utf-8');
+    return function (message) {
+        const appendageBytes = Buffer.from(appendage, 'base64');
+        const messageBytes = Buffer.from(message, 'utf-8');
 
-    const plaintextBuffer = Buffer.concat([messageBytes, appendageBytes]);
+        const plaintextBuffer = Buffer.concat([messageBytes, appendageBytes]);
 
-    const plaintextWA = convert_to_word_array(plaintextBuffer);
-    return AES_ECB_cipher(keyWA, plaintextWA);
-  };
+        const plaintextWA = convert_to_word_array(plaintextBuffer);
+        return AES_ECB_cipher(keyWA, plaintextWA);
+    };
+}
+
+function findKeySizeForEncryptionOracle(encryptionOracle) {
+    let keySize = 1;
+    const char = 'a';
+    let message = char;
+    let prevCipher = encryptionOracle(message).slice(0, keySize*2);
+    
+    while(keySize <= 32) {
+        keySize += 1;
+        message += char;
+        
+        const cipher = encryptionOracle(message).slice(0, keySize*2);
+
+        if(cipher.startsWith(prevCipher)) {
+            break;
+        }
+
+        prevCipher = cipher;
+    }
+
+    return keySize - 1;
 }
 
 module.exports = {
-  encryptionOracleFactoryECB
+    encryptionOracleFactoryECB
 };
